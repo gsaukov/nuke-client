b52194ef24daf8233e18c168b08564aa5f8d3f4d
Georgy Saukov <georgy.saukov@sap.com>
2023-12-09 02:28:04 +0100

    Nuke client random point function fix

--- src/app/services/random-point-in-polygon.function.ts
+++ src/app/services/random-point-in-polygon.function.ts
@@ -1,5 +1,62 @@
-import {Feature, MultiPolygon, Point, Polygon} from "@turf/turf";
+import {Feature, MultiPolygon, Point, Polygon, Position} from "@turf/turf";
 import * as turf from "@turf/turf";
+
+interface IPolygonDesc {
+  polygon: Feature<Polygon>,
+  area: number,
+  weight: number,
+}
+
+export function randomPointsInPolygon(polygon: Feature<(Polygon | MultiPolygon)>, num: number): Feature<Point>[] {
+  const points: Feature<Point>[] = []
+  const describedPolygons = describePolygons(polygon);
+  const length = describedPolygons.length;
+  let pointsCounter = num;
+  let i = 0
+  for(;;) {
+    const describedPolygon = describedPolygons[i];
+    if(weightedChance(describedPolygon)){
+      points.push(recursiveRandomPointInPolygon(describedPolygon.polygon))
+      pointsCounter = pointsCounter-1;
+      if (pointsCounter == 0) {
+        return points
+      }
+    }
+    i = infiniteCounter(i, length);
+  }
+}
+
+function infiniteCounter(i: number, length: number): number {
+  return   length === i+1 ? 0 : i+1
+}
+
+function weightedChance(describedPolygon: IPolygonDesc) {
+  return Math.random() < describedPolygon.weight;
+}
+
+function describePolygons(feature: Feature<(Polygon | MultiPolygon)>): IPolygonDesc[] {
+  const polygonsDesc: IPolygonDesc[] = []
+  let totalArea = 0
+  const polygons = feature.geometry.coordinates
+  for (let n = 0; n < polygons.length; n++) {
+    const polygonDesc = createPolygonDesc(polygons[n] as Position[][])
+    totalArea = totalArea + polygonDesc.area
+    polygonsDesc.push(polygonDesc)
+  }
+  polygonsDesc.forEach(e => e.weight = e.area/totalArea )
+  return polygonsDesc;
+}
+
+function createPolygonDesc(polygon: Position[][]):IPolygonDesc {
+  const turfPoligon = turf.polygon(polygon);
+  const area = turf.area(turfPoligon)
+  const weight = 0;
+  return {
+    polygon: turfPoligon,
+    area: area,
+    weight: weight,
+  }
+}
 
 export function recursiveRandomPointInPolygon(polygon: Feature<(Polygon | MultiPolygon)>, depth: number = 500): Feature<Point> {
   //bbox extent in minX, minY, maxX, maxY order
