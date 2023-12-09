7e10ec66d2c39bd60d060157488b4ddcebd50c1a
Georgy Saukov <georgy.saukov@sap.com>
2023-12-09 02:48:19 +0100

    Nuke client random point rework.

--- src/app/services/workers/random-points.function.ts
+++ src/app/services/workers/random-points.function.ts
@@ -1,15 +1,63 @@
-import {Feature, MultiPolygon, Point, Polygon} from "@turf/turf";
+import {Feature, MultiPolygon, Point, Polygon, Position} from "@turf/turf";
 import {recursiveRandomPointInPolygon} from "../random-point-in-polygon.function";
 import {RandomPointsWorkerProgress} from "./random-points.worker";
+import * as turf from "@turf/turf";
 
+interface IPolygonDesc {
+  polygon: Feature<Polygon>,
+  area: number,
+  weight: number,
+}
 export function randomPointsInPolygon(num: number, polygon: Feature<(Polygon | MultiPolygon)>): RandomPointsWorkerProgress {
   const points: Feature<Point>[] = []
-  for(let i = 0; i < num; i++ ) {
-    points[i] = recursiveRandomPointInPolygon(polygon)
-    if(i%100===0){
-      self.postMessage({isComplete: false, message: i, result: points});
+  const describedPolygons = describePolygons(polygon);
+  const length = describedPolygons.length;
+  let pointsCounter = num;
+  let i = 0
+  for(;;) {
+    const describedPolygon = describedPolygons[i];
+    if(weightedChance(describedPolygon)){
+      points.push(recursiveRandomPointInPolygon(describedPolygon.polygon))
+      pointsCounter = pointsCounter-1;
+      if (points.length % 100 == 0 || pointsCounter == 0) {
+        self.postMessage({isComplete: false, message: num, result: points});
+      }
+      if (pointsCounter == 0) {
+        return {isComplete: true, message: num, result: points}
+      }
     }
+    i = infiniteCounter(i, length);
   }
-  self.postMessage({isComplete: false, message: num, result: points});
-  return {isComplete: true, result: points};
+}
+
+function infiniteCounter(i: number, length: number): number {
+  return   length === i+1 ? 0 : i+1
+}
+
+function weightedChance(describedPolygon: IPolygonDesc) {
+  return Math.random() < describedPolygon.weight;
+}
+
+function describePolygons(feature: Feature<(Polygon | MultiPolygon)>): IPolygonDesc[] {
+  const polygonsDesc: IPolygonDesc[] = []
+  let totalArea = 0
+  const polygons = feature.geometry.coordinates
+  for (let n = 0; n < polygons.length; n++) {
+    const polygonDesc = createPolygonDesc(polygons[n] as Position[][])
+    totalArea = totalArea + polygonDesc.area
+    polygonsDesc.push(polygonDesc)
+  }
+  polygonsDesc.forEach(e => e.weight = e.area/totalArea )
+  return polygonsDesc;
+}
+
+function createPolygonDesc(polygon: Position[][]):IPolygonDesc {
+  const turfPoligon = turf.polygon(polygon);
+  const area = turf.area(turfPoligon)
+  const weight = 0;
+  return {
+    polygon: turfPoligon,
+    area: area,
+    weight: weight,
+  }
 }
