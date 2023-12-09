59d421a3b30840cdf1ba0da5c37857774390f5ff
Georgy Saukov <georgy.saukov@sap.com>
2023-12-09 03:07:54 +0100

    Nuke client random point rework.

--- src/app/services/turf.service.ts
+++ src/app/services/turf.service.ts
@@ -1,9 +1,6 @@
 import {Injectable} from '@angular/core';
 import * as turf from '@turf/turf';
 import { BBox } from "@turf/helpers";
-import {Feature, MultiPolygon, Polygon, Point} from "@turf/turf";
-import {Observable, of} from "rxjs";
-import {recursiveRandomPointInPolygon} from "./random-point-in-polygon.function";
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +8,6 @@ import {recursiveRandomPointInPolygon} from "./random-point-in-polygon.function"
 export class TurfService {
 
   constructor() {
-  }
-
-  public randomPointsInPolygon(num: number, polygon: Feature<(Polygon | MultiPolygon)>): Feature<Point>[] {
-    const points: Feature<Point>[] = []
-    for(let i = 0; i < num; i++ ) {
-      points[i] = recursiveRandomPointInPolygon(polygon)
-    }
-    return points
-  }
-
-  public randomPointInPolygon(polygon: Feature<(Polygon | MultiPolygon)>): Observable<Feature<Point>> {
-    return of(recursiveRandomPointInPolygon(polygon));
   }
 
   public bbox(geojson: any): BBox {
--- src/app/services/workers/random-points.function.ts
+++ src/app/services/workers/random-points.function.ts
@@ -1,5 +1,4 @@
 import {Feature, MultiPolygon, Point, Polygon, Position} from "@turf/turf";
-import {recursiveRandomPointInPolygon} from "../random-point-in-polygon.function";
 import {RandomPointsWorkerProgress} from "./random-points.worker";
 import * as turf from "@turf/turf";
 
@@ -26,11 +25,11 @@ export function randomPointsInPolygon(num: number, polygon: Feature<(Polygon | M
         return {isComplete: true, message: num, result: points}
       }
     }
-    i = infiniteCounter(i, length);
+    i = infiniteCyclicCounter(i, length);
   }
 }
 
-function infiniteCounter(i: number, length: number): number {
+function infiniteCyclicCounter(i: number, length: number): number {
   return   length === i+1 ? 0 : i+1
 }
 
@@ -59,5 +58,34 @@ function createPolygonDesc(polygon: Position[][]):IPolygonDesc {
     polygon: turfPoligon,
     area: area,
     weight: weight,
+  }
+}
+
+function recursiveRandomPointInPolygon(polygon: Feature<(Polygon | MultiPolygon)>, depth: number = 500): Feature<Point> {
+  //bbox extent in minX, minY, maxX, maxY order
+  if(depth <= 0) {
+    throw new Error('Random point calculation recursion depth limit reached')
+  }
+  const bounds = turf.bbox(polygon)
+  const x_min = bounds[0];
+  const y_min = bounds[1];
+  const x_max = bounds[2];
+  const y_max = bounds[3];
+
+  const lat = y_min + (Math.random() * (y_max - y_min));
+  const lng = x_min + (Math.random() * (x_max - x_min));
+
+  const point: Feature<Point> = turf.point([lng, lat]);
+  try {
+    var inside = turf.booleanPointInPolygon(point, polygon);
+  } catch (e) {
+    console.log(polygon)
+    console.log(point)
+    throw e
+  }
+  if (inside) {
+    return point
+  } else {
+    return recursiveRandomPointInPolygon(polygon, depth-1)
   }
 }
