9fda1bb32fa74a9cc89191466b967c16e13c28aa
Georgy Saukov <georgy.saukov@sap.com>
2023-12-15 20:47:29 +0100

    Nuke client place query component styles.

--- src/app/main-page/map-page/place-query/place-query.component.ts
+++ src/app/main-page/map-page/place-query/place-query.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {NominatimService} from "../../../services/nominatim.service";
 
 @Component({
   selector: 'app-place-query',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class PlaceQueryComponent {
 
+  form: FormGroup
+
+  constructor(private nominatimService: NominatimService) {
+    this.form = new FormGroup({
+      placeType: new FormControl(null, [Validators.required]),
+      placeName: new FormControl(null, [Validators.required]),
+    })
+  }
+
+  onSubmit() {
+
+  }
 }
