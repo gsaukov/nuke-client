06e3e15f44904f2b8e696fa83fc1d4cc80b3987a
Georgy Saukov <georgy.saukov@sap.com>
2023-12-22 23:45:30 +0100

    Nuke client place query results table design

--- src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
+++ src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
@@ -11,12 +11,24 @@ export class PlaceQueryResultsComponent {
 
   constructor() {
     this.dataSource = []
-    let o = {
+    let p1 = {
       number: 1,
+      placeName: "Usa, Oita Prefecture, Japan",
+      placeId: 36004004810,
+    }
+    let p2 = {
+      number: 2,
       placeName: "Usa, Shigonsky District, Samara Oblast, Volga Federal District, Russia",
       placeId: 36003427103,
     }
-    this.dataSource.push(o)
+    let p3 = {
+      number: 3,
+      placeName: "Usa, Wetteraukreis, Hesse, Germany",
+      placeId: 36010966429,
+    }
+    this.dataSource.push(p1)
+    this.dataSource.push(p2)
+    this.dataSource.push(p3)
   }
 
   ngOnInit() {
