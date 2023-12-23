e855cd93fb0397a5ff03e40b9792a681ed255963
Georgy Saukov <georgy.saukov@sap.com>
2023-12-23 23:20:29 +0100

    Nuke client place query component

--- src/app/main-page/map-page/place-query/place-query.component.ts
+++ src/app/main-page/map-page/place-query/place-query.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {NominatimService} from "../../../services/nominatim.service";
+import {of} from "rxjs";
+import {mergeMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-place-query',
@@ -14,7 +16,7 @@ export class PlaceQueryComponent {
   constructor(private nominatimService: NominatimService) {
     this.form = new FormGroup({
       queryType: new FormControl(null, [Validators.required]),
-      placeName: new FormControl(null, []),
+      placeQuery: new FormControl(null, []),
       countryName: new FormControl(null, []),
       stateName: new FormControl(null, []),
       countyName: new FormControl(null, []),
@@ -23,6 +25,16 @@ export class PlaceQueryComponent {
   }
 
   onSubmit() {
+    let queryObject = {
+      q: this.form.controls['placeQuery'].value,
+      country: this.form.controls['countryName'].value,
+      state: this.form.controls['stateName'].value,
+      county: this.form.controls['countyName'].value,
+      city: this.form.controls['cityName'].value,
+    }
 
+    of(queryObject).pipe(
+      mergeMap((queryObject) => this.nominatimService.searchPlaceQuery(queryObject)),
+    )
   }
 }
