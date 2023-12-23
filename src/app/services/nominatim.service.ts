207618707e8cbf8d7a61ffffc45ea141f382ea53
Georgy Saukov <georgy.saukov@sap.com>
2023-12-23 22:58:01 +0100

    Nuke client nominatim client query

--- src/app/services/nominatim.service.ts
+++ src/app/services/nominatim.service.ts
@@ -1,8 +1,16 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from "rxjs";
 
 const API_URL = 'https://nominatim.openstreetmap.org'
+
+export interface NominatimQuery {
+  city: string;
+  county: string;
+  state: string;
+  country: string;
+  q: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +22,11 @@ export class NominatimService {
 
   searchPlace(placeType: string, placeName: string): Observable<any> {
     return this.http.get<any>(`${API_URL}/search?X-Requested-With=overpass-turbo&format=json&${placeType}=${placeName}`)
+  }
+
+  searchPlaceQuery(queryObject: NominatimQuery): Observable<any> {
+    let query = new HttpParams({fromObject: queryObject as any}).toString()
+    return this.http.get<any>(`${API_URL}/search?X-Requested-With=overpass-turbo&format=json&${query}`)
   }
 
 }
