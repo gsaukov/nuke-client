b1aec0971c3714113347a0c13ddb39dfed66d956
Georgy Saukov <georgy.saukov@sap.com>
2023-12-25 22:35:14 +0100

    Nuke client place query results

--- src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
+++ src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {NominatimResult} from "../../../../services/nominatim.service";
 
 @Component({
   selector: 'app-place-query-results',
@@ -6,29 +7,10 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./place-query-results.component.css']
 })
 export class PlaceQueryResultsComponent {
-  @Input()dataSource: any[]
-  columnsToDisplay = ['#', 'placeName', 'placeId']
+  @Input()dataSource!: NominatimResult[]
+  columnsToDisplay = ['#', 'display_name', 'addresstype', 'osm_id', ]
 
   constructor() {
-    this.dataSource = []
-    let p1 = {
-      number: 1,
-      placeName: "Usa, Oita Prefecture, Japan",
-      placeId: 36004004810,
-    }
-    let p2 = {
-      number: 2,
-      placeName: "Usa, Shigonsky District, Samara Oblast, Volga Federal District, Russia",
-      placeId: 36003427103,
-    }
-    let p3 = {
-      number: 3,
-      placeName: "Usa, Wetteraukreis, Hesse, Germany",
-      placeId: 36010966429,
-    }
-    this.dataSource.push(p1)
-    this.dataSource.push(p2)
-    this.dataSource.push(p3)
   }
 
   ngOnInit() {
