ad7fec92bf9773ce86ac7064bce281a0c503db41
Georgy Saukov <georgy.saukov@sap.com>
2023-12-31 22:37:03 +0100

    Nuke client overpass country geometry

--- src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
+++ src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {NominatimResult} from "../../../../services/nominatim.service";
 import {OverpassService} from "../../../../services/overpass.service";
+import {mergeMap, of} from "rxjs";
+import * as osm2geojson from "osm2geojson-lite";
+import {MapService} from "../../../../services/map.service";
 
 @Component({
   selector: 'app-place-query-results',
@@ -11,7 +14,7 @@ export class PlaceQueryResultsComponent {
   private _dataSource!: NominatimResult[]
   columnsToDisplay = ['#', 'display_name', 'addresstype', 'osm_id', ]
 
-  constructor(private overpassService: OverpassService) { }
+  constructor(private overpassService: OverpassService, private mapService: MapService) { }
 
   @Input()
   set dataSource(dataSource: NominatimResult[]) {
@@ -24,6 +27,9 @@ export class PlaceQueryResultsComponent {
 
   selectPlace(row:NominatimResult) {
     const countryId = OverpassService.getOverpassCountryId(row.osm_id)
-    return this.overpassService.getGeometryData(countryId);
+    this.overpassService.getGeometryData(countryId).pipe(
+      mergeMap(overpassRes => of(osm2geojson(overpassRes, {completeFeature: true}))),
+      mergeMap(geoJsonObject => this.mapService.addGeometryLayer(geoJsonObject))
+    ).subscribe()
   }
 }
