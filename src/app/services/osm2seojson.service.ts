61b2254a916c3d1dcbae8bff11caa0f6109b8734
Georgy Saukov <georgy.saukov@sap.com>
2024-01-07 20:59:11 +0100

    Nuke client osmtogeojson way conversion fix.

--- src/app/services/osm2seojson.service.ts
+++ src/app/services/osm2seojson.service.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@angular/core';
-import * as osm2geojson from "osm2geojson-lite";
+import * as osmtogeojson from "osmtogeojson";
 import {FeatureCollection} from "@turf/helpers/dist/js/lib/geojson";
-
-
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +8,7 @@ import {FeatureCollection} from "@turf/helpers/dist/js/lib/geojson";
 export class Osm2GeojsonService {
 
   convert(overpassRes:any):FeatureCollection {
-    return osm2geojson(overpassRes, {completeFeature: true}) as FeatureCollection;
+    return osmtogeojson(overpassRes) as FeatureCollection;
   }
 
 }
