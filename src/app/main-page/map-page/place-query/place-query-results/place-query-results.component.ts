6d8c2f53a13ed24f6c9ef436cf4a07ffd70695ee
Georgy Saukov <georgy.saukov@sap.com>
2024-01-21 22:10:59 +0100

    Nuke client place events route

--- src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
+++ src/app/main-page/map-page/place-query/place-query-results/place-query-results.component.ts
@@ -2,8 +2,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NominatimResult} from "../../../../services/nominatim.service";
 import {ILayerID, LayersService} from "../../../../services/layers.service";
 import {FeatureCollection} from "@turf/helpers/dist/js/lib/geojson";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {GeojsonFlowService} from "../../../../services/geojson-flow.service";
+import {EMPTY, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-place-query-results',
@@ -18,7 +19,7 @@ export class PlaceQueryResultsComponent {
 
   @Output() redirect:EventEmitter<FeatureCollection> = new EventEmitter();
 
-  constructor(private flowService: GeojsonFlowService, private layersService: LayersService, private router:Router) { }
+  constructor(private flowService: GeojsonFlowService, private layersService: LayersService, private router:Router, private route: ActivatedRoute) { }
 
   @Input()
   set dataSource(dataSource: NominatimResult[]) {
@@ -29,25 +30,33 @@ export class PlaceQueryResultsComponent {
     return this._dataSource;
   }
 
-  previewPlace(row:NominatimResult) {
+  previewPlace(row:NominatimResult): Observable<void> {
     if(!this.isSameLayer(row)){
       this.removePreviewLayer()
       this.flowService.previewPlace(row).subscribe(
         responses => {
           this.previewPlaceLayerId = responses[0]
           this.previewGeoJson = responses[1]
+          return EMPTY
         }
+      )
+    }
+    return EMPTY
+  }
+
+  selectPlace(row:NominatimResult) {
+    if(this.isSameLayer(row)){
+      this.redirectToEvents()
+    } else {
+      this.previewPlace(row).subscribe(
+        () => this.redirectToEvents()
       )
     }
   }
 
-  selectPlace(row:NominatimResult) {
-    if(this.previewPlaceLayerId && this.previewPlaceLayerId.geoJsonId.indexOf(row.osm_id.toString()) > 0){
-      this.redirect.emit(this.previewGeoJson);
-      this.router.navigate(["url"]);
-    } else {
-      this.previewPlace(row)
-    }
+  private redirectToEvents() {
+    this.redirect.emit(this.previewGeoJson);
+    this.router.navigate(["events"], { relativeTo: this.route });
   }
 
   private isSameLayer(row:NominatimResult):boolean {
